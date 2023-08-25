70a7389db29c14ff541b14bb60f607e67467351b
Erick Silva <ericksilvaredes@gmail.com>
2023-08-25 16:31:02 -0300

    Excepiton Handler - findById

---
+++ src/main/java/com/eiericksilvagroup/gestao/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.eiericksilvagroup.gestao.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
