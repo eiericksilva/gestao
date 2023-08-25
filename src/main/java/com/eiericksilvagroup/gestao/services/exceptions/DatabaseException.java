751c29b00509d8bf8edc51af07606a7680071953
Erick Silva <ericksilvaredes@gmail.com>
2023-08-25 16:52:06 -0300

    exception handler - delete

---
+++ src/main/java/com/eiericksilvagroup/gestao/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.eiericksilvagroup.gestao.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
